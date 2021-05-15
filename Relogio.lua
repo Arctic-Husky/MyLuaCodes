348078d7dbf1a4410d2717d0e84a61acdf6edb72
Caio Schmidt <caiofonseca6@gmail.com>
2021-05-15 16:54:27 -0300

    Create Relogio.lua

---
+++ Relogio.lua
@@ -0,0 +1,14 @@
+time = os.time()
+
+--m = peripheral.wrap("left)
+
+while(true) do
+  --local timeInt = math.floor(time)
+  print(math.floor(time) .. " horas")
+  
+  time = os.time()
+  
+  sleep(1)
+  term.clear()
+  
+end
